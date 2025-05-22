d043d098ef4fb1b8606d9a72ccaa6d087b260038
sugiouga <sugiouga@gmail.com>
2025-05-22 19:10:27 +0900

    まずvehicleを作成する

---
+++ Simulation.m
@@ -0,0 +1,26 @@
+classdef Simulation
+    % シミュレーションの起動･ステップの実行･終了を行うクラス
+    properties
+        % シミュレーションの実行時間
+        time = [];
+        % シミュレーションの開始時間
+        START_TIME = 0;
+        % シミュレーションの終了時間
+        END_TIME = 60;
+        % シミュレーションの時間間隔
+        TIME_STEP = 0.01;
+        % シミュレーションの終了フラグ
+        isEnd = false;
+    end
+
+    methods
+
+        function obj = Simulation()
+            % コンストラクタ
+            % シミュレーションの時間を初期化
+            obj.time = obj.START_TIME;
+
+        end
+
+    end
+end
